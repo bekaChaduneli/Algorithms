d0e42aa86a6a0fdb9f6dfb16dffac35f8956dfff
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-01 18:47:42 +0400

    initial commit

---
+++ primeNumber.js
@@ -0,0 +1,13 @@
+function isPrime(n) {
+    if (n < 2) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % 2 === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Big-O = O(sqrt(n))
