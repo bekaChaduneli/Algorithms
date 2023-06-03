49626dbf87c4b7c72dd9b9d5db1af7c1557c14b6
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-03 22:32:40 +0400

    recursive fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,23 @@
 function fibonacci(n) {
-    const fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    return fib;
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
 }
+
+// reacursion fibonacci
+
+function recursiveFibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+console.log(recursiveFibonacci(0));
+console.log(recursiveFibonacci(1));
+console.log(recursiveFibonacci(6));
+
+// O(n) - iterative
+// O(2^n) - Recursive
