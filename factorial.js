e4ebd67e5652acb7a0e212553078204568b8d340
Beka Chaduneli <beka.chaduneli.1@btu.edu.ge>
2023-06-03 23:16:18 +0400

    linearSearch algorithm

--- factorial.js
+++ factorial.js
@@ -1,7 +1,18 @@
 function factorial(n) {
-    let result = 1;
-    for (let i = 2; i < n; i++) {
-        result = result * i;
-    }
-    return result;
+  let result = 1;
+  for (let i = 2; i < n; i++) {
+    result = result * i;
+  }
+  return result;
 }
+
+//recursive
+
+function recursiveFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
+}
+
+// Big-O = O(n)
